4e8766e0cee1d086013afa12a00c445cf8ebb0dd
Fedor Chukharev <fedor.s.chukharev@gmail.com>
2019-10-01 02:24:24 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ParallelRecognition1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dirPath = Console.ReadLine();
+            ParallelRecognition parallelRecognition = new ParallelRecognition(dirPath);
+            parallelRecognition.Run();
+        }
+    }
+}
---
+++ ParallelRecognition/ParallelRecognition.cs
@@ -0,0 +1,85 @@
+﻿//variant a2
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics.Tensors;
+using Microsoft.ML.OnnxRuntime;
+
+namespace ParallelRecognition1
+{
+    public class ParallelRecognition
+    {
+        private string directoryPath;
+        public string DirectoryPath { get => directoryPath; set => directoryPath = value; }
+
+        public bool HasFinished { get; private set; }
+
+        public ParallelRecognition(string directoryPath)
+        {
+            DirectoryPath = directoryPath;
+        }
+
+        public bool Run()
+        {
+            var filePaths = Directory.GetFiles(DirectoryPath);
+            foreach (var filePath in filePaths)
+            {
+                UseApi(filePath);
+            }
+            return true;
+        }
+
+        public bool Stop()
+        {
+            return true;
+        }
+
+        static void UseApi(string filePath)
+        {
+            string modelPath = @"ResNet50.onnx";
+            using (var session = new InferenceSession(modelPath))
+            {
+                var inputMeta = session.InputMetadata;
+                var container = new List<NamedOnnxValue>();
+
+                float[] inputData = LoadTensorFromFile(filePath); // this is the data for only one input tensor for this model
+
+                foreach (var name in inputMeta.Keys)
+                {
+                    var tensor = new DenseTensor<float>(inputData, inputMeta[name].Dimensions);
+                    container.Add(NamedOnnxValue.CreateFromTensor<float>(name, tensor));
+                }
+
+                // Run the inference
+                using (var results = session.Run(container))  // results is an IDisposableReadOnlyCollection<DisposableNamedOnnxValue> container
+                {
+                    // dump the results
+                    foreach (var r in results)
+                    {
+                        Console.WriteLine("Output for {0}", r.Name);
+                        Console.WriteLine(r.AsTensor<float>().GetArrayString());
+                    }
+                }
+            }
+        }
+
+        static float[] LoadTensorFromFile(string filename)
+        {
+            var tensorData = new List<float>();
+
+            // read data from file
+            using (var inputFile = new System.IO.StreamReader(filename))
+            {
+                inputFile.ReadLine(); //skip the input name
+                string[] dataStr = inputFile.ReadLine().Split(new char[] { ',', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < dataStr.Length; i++)
+                {
+                    tensorData.Add(Single.Parse(dataStr[i]));
+                }
+            }
+
+            return tensorData.ToArray();
+        }
+
+    }
+}
