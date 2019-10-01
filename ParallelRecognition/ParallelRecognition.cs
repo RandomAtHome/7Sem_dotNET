3b48dad090d759e3006a12132b105ddbf34ceb03
Fedor Chukharev <fedor.s.chukharev@gmail.com>
2019-10-01 06:32:13 +0300

    More parallel code

--- ParallelRecognition/ParallelRecognition.cs
+++ ParallelRecognition/ParallelRecognition.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Numerics.Tensors;
+using System.Threading;
 using Microsoft.ML.OnnxRuntime;
 
 namespace ParallelRecognition
@@ -10,23 +11,59 @@ namespace ParallelRecognition
     public class ParallelRecognition
     {
         private string directoryPath;
-        public string DirectoryPath { get => directoryPath; set => directoryPath = value; }
+        public string DirectoryPath { get => directoryPath; private set => directoryPath = value; }
 
         public bool HasFinished { get; private set; }
+        bool IsInterrupted { get; set; }
+        Thread manager_thread = null;
 
         public ParallelRecognition(string directoryPath)
         {
             DirectoryPath = directoryPath;
+            manager_thread = new Thread(ManageJobs);
         }
 
         public bool Run()
         {
-            var filePaths = Directory.GetFiles(DirectoryPath);
-            foreach (var filePath in filePaths)
-            {
-                RecognizeContentsStub(filePath);
-            }
+            HasFinished = false;
+            IsInterrupted = false;
+            manager_thread.Start(Directory.GetFiles(DirectoryPath));
             return true;
+        }
+
+        void ManageJobs(object filenames)
+        {
+            var files = filenames as string[];
+            int fileIndex = 0;
+            ThreadPool.GetMaxThreads(out int workerThreadsCount, out int portThreads);
+            var workers = new Thread[workerThreadsCount];
+            for (int i = 0; i < workers.Length; i++)
+            {
+                workers[i] = new Thread(RecognizeContentsStub)
+                {
+                    IsBackground = true
+                };
+            }
+            while (fileIndex < files.Length)
+            {
+                if (IsInterrupted) break;
+                for (int i = 0; i < workers.Length; i++)
+                {
+                    if (!workers[i].IsAlive && !IsInterrupted)
+                    {
+                        workers[i] = new Thread(RecognizeContentsStub)
+                        {
+                            IsBackground = true
+                        };
+                        workers[i].Start(files[fileIndex++]);
+                    }
+                }
+            }
+            foreach (var worker in workers)
+            {
+                worker.Join();
+            }
+            HasFinished = true;
         }
 
         public bool Stop()
@@ -34,9 +71,10 @@ namespace ParallelRecognition
             return true;
         }
 
-        static string RecognizeContentsStub(string filePath)
+        static void RecognizeContentsStub(object obj)
         {
-            return "{'Yay': 0}";
+            var filePath = obj as string;
+            //return "{'Yay': 0}";
         }
 
             static void RecognizeContents(string filePath)
