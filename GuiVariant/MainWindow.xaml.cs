4b2c696a04da75dbc2c1b01fbf091fd9b10297fa
Fedor Chukharev <fedor.s.chukharev@gmail.com>
2019-10-29 09:18:23 +0300

    Some more content

--- GuiVariant/MainWindow.xaml.cs
+++ GuiVariant/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ParallelRecognition;
 
 namespace GuiVariant
 {
     public partial class MainWindow : Window
     {
+        private ParallelRecognition.ParallelRecognition parallelRecognition = null;
         private string directoryPath = null;
         string DirectoryPath { get => directoryPath; set => directoryPath = value; }
 
@@ -28,7 +31,7 @@ namespace GuiVariant
             InitializeComponent();
             if (TryFindResource("key_ObsClassInfo") is ObservableClassInfo tmp)
             {
-                tmp.Add("Test");
+                tmp.Add(new ClassInfo() { ClassName = "Test", Count = 3});
             };
             if (TryFindResource("key_ObsImageItems") is ObservableImageItem tmp2)
             {
@@ -45,7 +48,7 @@ namespace GuiVariant
                     e.Accepted = true;
                     return;
                 }
-                e.Accepted = classesDataList.SelectedItem.ToString() == (e.Item as ImageItem).PredictedClass;
+                e.Accepted = (classesDataList.SelectedItem as ClassInfo).ClassName == (e.Item as ImageItem).PredictedClass;
             } else
             {
                 e.Accepted = false;
@@ -56,15 +59,52 @@ namespace GuiVariant
         {
             (FindResource("key_FilteredView") as CollectionViewSource).View.Refresh();
         }
+
+        private void directorySelectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK || result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    DirectoryPath = dialog.SelectedPath;
+                    startRecognitionBtn.IsEnabled = true;
+                    directorySelectBtn.IsEnabled = false;
+
+                    parallelRecognition = new ParallelRecognition.ParallelRecognition(DirectoryPath);
+                    var images = (FindResource("key_ObsImageItems") as ObservableImageItem);
+                    images.Clear();
+                    foreach (var filePath in Directory.GetFiles(DirectoryPath))
+                    {
+                        images.Add(new ImageItem
+                        {
+                            Image = new BitmapImage(new Uri(filePath)),
+                            PredictedClass = "TBD",
+                            ImagePath = filePath
+                        });
+                    }
+                }
+            }
+        }
     }
 
     public class ObservableImageItem : ObservableCollection<ImageItem> {}
-    public class ObservableClassInfo : ObservableCollection<string> {}
+    public class ObservableClassInfo : ObservableCollection<ClassInfo> {}
 
     public class ImageItem
     {
         public BitmapImage Image { get; set; }
         public string ImagePath { get; set; }
         public string PredictedClass { get; set; }
+    }
+
+    public class ClassInfo
+    {
+        public string ClassName { get; set; }
+        public int Count { get; set; }
+        public override string ToString()
+        {
+            return ClassName + " | " + Count.ToString();
+        }
     }
 }
