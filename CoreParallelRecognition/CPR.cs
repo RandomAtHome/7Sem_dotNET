a26035aad22c949f778c8b2b9061c1f1346c1cfc
Fedor Chukharev <fedor.s.chukharev@gmail.com>
2019-12-09 20:41:51 +0300

    Rewrite DLL to worker over a single file that was sent

--- CoreParallelRecognition/CPR.cs
+++ CoreParallelRecognition/CPR.cs
@@ -14,133 +14,77 @@ namespace CoreParallelRecognition
 {
     public class ParallelRecognition
     {
-        readonly ManualResetEvent hasFinishedEvent = new ManualResetEvent(true);
+        static readonly ManualResetEvent limitHit = new ManualResetEvent(true);
+        static private readonly int threadLimit = 4;
+        static private volatile int curThreads = 0;
 
-        bool IsInterrupted { get; set; } = false;
-        public bool HasFinished { get; private set; } = true;
-        public string DirectoryPath { get; private set; }
-        public ConcurrentQueue<ImageClassified> CreationTimes { get; } = new ConcurrentQueue<ImageClassified>();
-
-        public ParallelRecognition(string directoryPath)
+        static public ImageClassified RecognizeContents(byte[] fileBytes, string filename)
         {
-            DirectoryPath = directoryPath;
-        }
-
-        public bool Run()
-        {
-            try
-            {
-                new Thread(ManageJobs)
+            using var session = new InferenceSession(@"C:\Users\randomnb\Desktop\DnnImageModels\ResNet50Onnx\resnet50v2.onnx");
+            var inputMeta = session.InputMetadata;
+            var container = new List<NamedOnnxValue>();
+            var tensor = LoadTensorFromFileBytes(fileBytes);
+            { // to hide scope
+                if (FindInDB(fileBytes) is ImageClassified imageClassified)
                 {
-                    IsBackground = true
-                }.Start(Directory.GetFiles(DirectoryPath));
-            }
-            catch (ArgumentException)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        void ManageJobs(object filenames)
-        {
-            hasFinishedEvent.Reset();
-            HasFinished = false;
-            IsInterrupted = false;
-            var queue = new ConcurrentQueue<string>(filenames as string[]);
-            int workerThreadsCount = Environment.ProcessorCount;
-            var workers = new Thread[workerThreadsCount];
-            for (int i = 0; i < workers.Length; i++)
-            {
-                workers[i] = new Thread(RecognizeContents)
-                {
-                    IsBackground = true
-                };
-                workers[i].Start(queue);
-            }
-            foreach (var worker in workers)
-            {
-                if (worker.IsAlive) worker.Join();
-            }
-            HasFinished = true;
-            IsInterrupted = false;
-            hasFinishedEvent.Set();
-        }
-
-        public bool Stop()
-        {
-            IsInterrupted = !HasFinished;
-            hasFinishedEvent.WaitOne();
-            return true;
-        }
-
-        void RecognizeContents(object obj)
-        {
-            var queue = obj as ConcurrentQueue<string>;
-            using (var session = new InferenceSession(@"C:\Users\randomnb\Desktop\DnnImageModels\ResNet50Onnx\resnet50v2.onnx"))
-            {
-                while (queue.TryDequeue(out string filePath))
-                {
-                    var inputMeta = session.InputMetadata;
-                    var container = new List<NamedOnnxValue>();
-                    if (IsInterrupted) break;
-                    var fileBytes = FileByNameToByteArray(filePath);
-                    var tensor = LoadTensorFromFile(filePath);
-                    { // to hide scope
-                        if (FindInDB(tensor, fileBytes) is ImageClassified imageClassified)
-                        {
-                            imageClassified.ImagePath = filePath;
-                            CreationTimes.Enqueue(imageClassified);
-                            continue;
-                        }
-                    }
-                    foreach (var name in inputMeta.Keys)
-                    {
-                        container.Add(NamedOnnxValue.CreateFromTensor<float>(name, tensor));
-                    }
-                    if (IsInterrupted) break;
-                    using (var results = session.Run(container))
-                    {
-                        foreach (var r in results)
-                        {
-                            double[] exp = new double[r.AsEnumerable<float>().Count()];
-                            int i = 0;
-                            foreach (var val in r.AsEnumerable<float>())
-                            {
-                                exp[i++] = Math.Exp(val);
-                            }
-                            var softmax = exp.Select(j => j / exp.Sum()).ToArray();
-                            var max_val1 = softmax.Max();
-                            var max_ind1 = Array.IndexOf(softmax, max_val1);
-                            var tensorHash = GetTensorHash(fileBytes);
-                            using (var db = new RecognitionModelContainer())
-                            {
-                                db.Results.Add(new Results()
-                                {
-                                    ClassId = max_ind1,
-                                    Probability = max_val1,
-                                    FileHash = tensorHash,
-                                    Filename = filePath,
-                                    Blob = new Blobs()
-                                    {
-                                        FileContent = fileBytes
-                                    },
-                                });
-                                db.SaveChanges();
-                            }
-                            CreationTimes.Enqueue(new ImageClassified()
-                            {
-                                ImagePath = filePath,
-                                ClassName = max_ind1.ToString(),
-                                Certainty = max_val1,
-                            });
-                        }
-                    }
+                    imageClassified.ImagePath = filename;
+                    return imageClassified;
                 }
             }
+            foreach (var name in inputMeta.Keys)
+            {
+                container.Add(NamedOnnxValue.CreateFromTensor<float>(name, tensor));
+            }
+            curThreads++;
+            if (curThreads > threadLimit)
+            {
+                limitHit.Reset();
+                limitHit.WaitOne();
+            }
+            using var results = session.Run(container);
+            foreach (var r in results)
+            {
+                double[] exp = new double[r.AsEnumerable<float>().Count()];
+                int i = 0;
+                foreach (var val in r.AsEnumerable<float>())
+                {
+                    exp[i++] = Math.Exp(val);
+                }
+                var softmax = exp.Select(j => j / exp.Sum()).ToArray();
+                var max_val1 = softmax.Max();
+                var max_ind1 = Array.IndexOf(softmax, max_val1);
+                var tensorHash = GetTensorHash(fileBytes);
+                using (var db = new RecognitionModelContainer())
+                {
+                    db.Results.Add(new Results()
+                    {
+                        ClassId = max_ind1,
+                        Probability = max_val1,
+                        FileHash = tensorHash,
+                        Filename = filename,
+                        Blob = new Blobs()
+                        {
+                            FileContent = fileBytes
+                        },
+                    });
+                    db.SaveChanges();
+                }
+                curThreads--;
+                if (curThreads <= threadLimit)
+                {
+                    limitHit.Reset();
+                }
+                return new ImageClassified()
+                {
+                    ImagePath = filename,
+                    ClassName = max_ind1.ToString(),
+                    Certainty = max_val1,
+                };
+            }
+            throw new Exception();
         }
 
-        private ImageClassified FindInDB(DenseTensor<float> tensor, byte[] fileBytes)
+        static private ImageClassified FindInDB(byte[] fileBytes)
         {
             ImageClassified result = null;
             var tensorHash = GetTensorHash(fileBytes);
@@ -168,19 +112,14 @@ namespace CoreParallelRecognition
             return result;
         }
 
-        private byte[] FileByNameToByteArray(string filename)
-        {
-            return File.ReadAllBytes(filename);
-        }
-
         static Int64 GetTensorHash(byte[] filebytes)
         {
             return new BigInteger(filebytes).GetHashCode();
         }
 
-        static DenseTensor<float> LoadTensorFromFile(string filename)
+        static DenseTensor<float> LoadTensorFromFileBytes(byte[] fileBytes)
         {
-            Bitmap tmp = new Bitmap(filename);
+            Bitmap tmp = new Bitmap(new MemoryStream(fileBytes));
             Bitmap img = new Bitmap(tmp, new Size(224, 224));
             float[,,,] data = new float[1, 3, img.Height, img.Width];
             float[] mean = new float[3] { 0.485F, 0.456F, 0.406F };
