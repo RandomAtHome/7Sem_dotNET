6953eb4a28f3844b80d1be12e9d17e751a1a3eaa
Fedor Chukharev <fedor.s.chukharev@gmail.com>
2019-12-09 18:01:31 +0300

    Add initial Server commit

---
+++ RESTService/Controllers/RecognizeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RESTService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecognizeController : ControllerBase
+    {
+        [HttpGet]
+        public DateTime Get()
+        {
+            return DateTime.Now;
+        }
+    }
+}
